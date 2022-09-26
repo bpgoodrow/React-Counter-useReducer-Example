7c8cee4241b95875b4c62dd07dc11871560f47a4
Ben Goodrow <bpgoodrow@gmail.com>
2022-09-26 11:27:35 -0700

    initial commit
    Co-authored-by: Winnie Wang
    <weijwang18@gmail.com>

---
+++ src/Counter.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useReducer } from 'react';
+
+const initialState = {
+    counter: 0
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'increment':
+            return{
+                counter: state.counter + 1
+            };
+        case 'decrement':
+          return{
+            counter: state.counter - 1
+          }; 
+          case 'resetToZero':
+            return{
+              counter: state.counter = 0
+            }; 
+        default:
+                throw new Error(`There is no action matching ${action.type}.`);
+    }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    document.title = state.counter;
+  }, [state.counter]);
+
+  return (
+    <React.Fragment>
+      {hidden ? <h1>Count Hidden</h1> : <h1>{state.counter}</h1>}
+      <button onClick={() => dispatch({type: 'increment'})}>Count!</button>
+      <button onClick={() => dispatch({type: 'decrement'})}>Decrement!</button>
+      <button onClick={() => dispatch({type: 'resetToZero'})}>Clear!</button>
+      <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
+    </React.Fragment>
+  );
+}
+
+export default Counter;
